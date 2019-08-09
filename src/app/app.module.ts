d3dc1a5b6ad3fe9ba84147f65f11222650a81d06
Maks <MaksymMaksymeiu@gmail.com>
2019-08-09 18:16:31 +0300

    add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatInputModule} from '@angular/material/input';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { MatButtonModule } from "@angular/material";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatInputModule } from "@angular/material/input";
+import { MatCardModule } from "@angular/material/card";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { InputTodoComponent } from './input-todo/input-todo.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { InputTodoComponent } from "./input-todo/input-todo.component";
+import { TodoListComponent } from "./todo-list/todo-list.component";
+import { TodoItemComponent } from "./todo-item/todo-item.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputTodoComponent
+    InputTodoComponent,
+    TodoListComponent,
+    TodoItemComponent
   ],
   imports: [
+    MatCardModule,
     MatInputModule,
     BrowserAnimationsModule,
     MatButtonModule,
@@ -23,4 +29,4 @@ import { InputTodoComponent } from './input-todo/input-todo.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
