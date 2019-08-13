439d619a933d7208d8d9aaaf07f87d110f2ef71c
Maks <MaksymMaksymeiu@gmail.com>
2019-08-13 17:54:40 +0300

    add material Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,39 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { MatButtonModule } from "@angular/material";
+import { FormsModule ,ReactiveFormsModule} from '@angular/forms';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatInputModule } from "@angular/material/input";
-import { MatCardModule } from "@angular/material/card";
 import { ClickOutsideModule } from 'ng-click-outside';
-
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { InputTodoComponent } from "./input-todo/input-todo.component";
 import { TodoListComponent } from "./todo-list/todo-list.component";
 import { TodoItemComponent } from "./todo-item/todo-item.component";
+import { NgModule } from '@angular/core';
 
+
+import { MatButtonModule, MatDatepickerModule,MatNativeDateModule } from "@angular/material";
+import { MatInputModule } from "@angular/material/input";
+import { MatCardModule } from "@angular/material/card";
+import {MatBadgeModule} from "@angular/material/badge"
 @NgModule({
   declarations: [
     AppComponent,
     InputTodoComponent,
     TodoListComponent,
-    TodoItemComponent
+    TodoItemComponent,
   ],
   imports: [
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    MatBadgeModule,
+    MatDatepickerModule,
     ClickOutsideModule,
     MatCardModule,
     MatInputModule,
     BrowserAnimationsModule,
     MatButtonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
