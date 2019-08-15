049ab9d8ffdb14b608429c7b3b4d2a1cb6349309
Maks <MaksymMaksymeiu@gmail.com>
2019-08-15 18:18:45 +0300

    add time picker

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,8 +1,11 @@
+import { TimeTrackService } from './../time-track.service';
 import { TodoService } from './../todo.service';
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
 import { Todo } from '../todo';
 import { FormControl } from '@angular/forms';
+import * as moment from 'moment';
+import 'moment/locale/pt-br';
 
 @Component({
   selector: 'app-dialog',
@@ -10,15 +13,21 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./dialog.component.scss']
 })
 export class DialogComponent implements OnInit {
-  serializedDate = new FormControl(new Date());
+  
+  serializedDate = new FormControl(Date());
+  serializedTime = new FormControl(Date());
   constructor(
+    public timeTrackService:TimeTrackService,
     private todoService:TodoService,
     @Inject(MAT_DIALOG_DATA) public data: Todo
   ) { }
 
  
   saveChages(newName){
-    this.data.deadLine = this.serializedDate.value;
+    console.log(`moment: ${moment(+this.serializedDate.value + this.timeTrackService.convertHours(this.serializedTime.value))}`)
+    const Deadline = new Date(+this.serializedDate.value + this.timeTrackService.convertHours(this.serializedTime.value));
+    console.log(`Deadline:${Deadline}`)
+    this.data.deadLine = Deadline;
     this.data.title = newName.value;
     this.todoService.update(this.data.id,this.data)
     this.todoService.dialog.closeAll()   
