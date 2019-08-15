06140c9939974f6b25d763864640a8315dca5722
Maks <MaksymMaksymeiu@gmail.com>
2019-08-15 15:49:43 +0300

    add date picker input

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -2,6 +2,7 @@ import { TodoService } from './../todo.service';
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
 import { Todo } from '../todo';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dialog',
@@ -9,12 +10,15 @@ import { Todo } from '../todo';
   styleUrls: ['./dialog.component.scss']
 })
 export class DialogComponent implements OnInit {
+  serializedDate = new FormControl(new Date());
   constructor(
     private todoService:TodoService,
     @Inject(MAT_DIALOG_DATA) public data: Todo
   ) { }
 
-  changeName(newName){
+ 
+  saveChages(newName){
+    this.data.deadLine = this.serializedDate.value;
     this.data.title = newName.value;
     this.todoService.update(this.data.id,this.data)
     this.todoService.dialog.closeAll()   
